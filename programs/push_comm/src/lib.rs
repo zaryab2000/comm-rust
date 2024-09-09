737281f9f8cf3d4b2b244cf6f40390ee4c3bc667
Zaryab <zaryabafser2000@gmail.com>
2024-09-09 16:41:59 +0400

    first commit

---
+++ programs/push_comm/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3F2sJzYnEQUt7J3MERtFCHKrz1VDQoFTg1gmZFxk9c86");
+
+#[program]
+pub mod push_comm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
