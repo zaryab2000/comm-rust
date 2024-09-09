2729d600ba9309d9cf0276e93543497a56f57c2e
Zaryab <zaryabafser2000@gmail.com>
2024-09-09 19:08:49 +0400

    added initializer

--- programs/push_comm/src/lib.rs
+++ programs/push_comm/src/lib.rs
@@ -1,6 +1,10 @@
 use anchor_lang::prelude::*;
+use core::mem::size_of;
+
 //import custom files
 mod state;
+use crate::state::PushCommStorageV3;
+
 
 
 declare_id!("3F2sJzYnEQUt7J3MERtFCHKrz1VDQoFTg1gmZFxk9c86");
@@ -9,10 +13,31 @@ declare_id!("3F2sJzYnEQUt7J3MERtFCHKrz1VDQoFTg1gmZFxk9c86");
 pub mod push_comm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, 
+        push_admin: Pubkey, 
+        chain_name: String,
+        chain_id: u64,
+    ) -> Result<()> {
+        let storage = &mut ctx.accounts.storage;
+        storage.governance = push_admin;
+        storage.push_channel_admin = push_admin;
+        storage.chain_id = chain_id;
+        storage.chain_name = chain_name;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize <'info>{
+    #[account(init,
+        payer = user,
+        space = size_of::<PushCommStorageV3>() + 8,
+        seeds = [],
+        bump)]
+    pub storage: Account<'info, PushCommStorageV3>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
